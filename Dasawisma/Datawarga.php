b87bbd215d3ccd172b1fc4bc9d329f68e5fe2521
sri-rita-utami <tata04@mhs.unsyiah.ac.id>
2021-05-20 09:14:32 +0700

    Mengupdate Halaman Tabel Data Dasawisma

--- Dasawisma/Datawarga.php
+++ Dasawisma/Datawarga.php
@@ -1,78 +1,81 @@
 <?php
 
-    //Memanggil Koneksi Database
-    $server = "localhost";
-    $user = "root";
-    $password = "";
-    $database = "dasawisma";
+//Memanggil Koneksi Database
+$server = "localhost";
+$user = "root";
+$password = "";
+$database = "dasawisma";
 
-    $koneksi = mysqli_connect($server, $user, $password, $database)or die(mysqli_error($koneksi));
-    ?>
+$koneksi = mysqli_connect($server, $user, $password, $database) or die(mysqli_error($koneksi));
+?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Data Warga</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
+
 <body>
-<div class="container">
+    <div class="container">
 
-    <!-- Awal Card Tabel -->
-    <div class="card mt-3" style="background-color: rgba(231, 116, 185, 0.6);">
-      <div class="card-header text-black text-center">
-        Data Warga
-      </div>
-      <div class="card-body">
-        
-        <table class="table table-bordered table-striped">
-        <style type="text/css">
-        table td {
-            border-width: 1px;
-            padding: 8px;
-            border-style: solid;
-            border-color: black;
-            background-color: rgba(0,0,0,.03);
-        }
-        </style>
-            <tr>
-                <th>No.</th>
-                <th>No. KK</th>
-                <th>Kepala Keluarga</th>
-                <th>Dasawisma</th>
-                <th>Lingkungan</th>
-                <th>Kelurahan</th>
-                <th>Kecamatan</th>
-                <th>Pilihan</th>
-            </tr>
-            <?php
-                $no = 1;
-                $tampil = mysqli_query($koneksi, "SELECT * from warga order by idwarga desc");
-                while($data = mysqli_fetch_array($tampil)){
+        <!-- Awal Card Tabel -->
+        <div class="card mt-3" style="background-color: rgba(231, 116, 185, 0.6);">
+            <div class="card-header text-black text-center">
+                Data Warga
+            </div>
+            <div class="card-body">
 
-            ?>
-            <tr>
-                <td><?=$no++;?></td>
-                <td><?=$data['nokk']?></td>
-                <td><?=$data['kepalakeluarga']?></td>
-                <td><?=$data['dasawisma']?></td>
-                <td><?=$data['lingkungan']?></td>
-                <td><?=$data['kelurahan']?></td>
-                <td><?=$data['kecamatan']?></td>
-                <td>
-                    <a href="editwarga.php?hal=edit&id=<?=$data['idwarga']?>" class="btn btn-warning"> Edit </a>
-                    <a href="index.php?hal=hapus&id=<?=$data['idwarga']?>" 
-                       onclick="return confirm('Apakah yakin ingin menghapus data ini?')" class="btn btn-danger"> Hapus </a>
-                </td>
-            </tr>
-        <?php } //penutup perulangan while ?>
-        </table>
+                <table class="table table-bordered table-striped">
+                    <style type="text/css">
+                        table td {
+                            border-width: 1px;
+                            padding: 8px;
+                            border-style: solid;
+                            border-color: black;
+                            background-color: rgba(0, 0, 0, .03);
+                        }
+                    </style>
+                    <tr>
+                        <th>No.</th>
+                        <th>No. KK</th>
+                        <th>Kepala Keluarga</th>
+                        <th>Dasawisma</th>
+                        <th>Lingkungan</th>
+                        <th>Kelurahan</th>
+                        <th>Kecamatan</th>
+                        <th>Pilihan</th>
+                    </tr>
+                    <?php
+                    $no = 1;
+                    $tampil = mysqli_query($koneksi, "SELECT * from warga order by idwarga desc");
+                    while ($data = mysqli_fetch_array($tampil)) {
 
-      </div>
+                    ?>
+                        <tr>
+                            <td><?= $no++; ?></td>
+                            <td><?= $data['nokk'] ?></td>
+                            <td><?= $data['kepalakeluarga'] ?></td>
+                            <td><?= $data['dasawisma'] ?></td>
+                            <td><?= $data['lingkungan'] ?></td>
+                            <td><?= $data['kelurahan'] ?></td>
+                            <td><?= $data['kecamatan'] ?></td>
+                            <td>
+                                <a href="editwarga.php?hal=edit&id=<?= $data['idwarga'] ?>" class="btn btn-warning"> Edit </a>
+                                <a href="hapuswarga.php?hal=hapus&id=<?= $data['idwarga'] ?>" onclick="return confirm('Apakah yakin ingin menghapus data ini?')" class="btn btn-danger"> Hapus </a>
+                            </td>
+                        </tr>
+                    <?php } //penutup perulangan while 
+                    ?>
+                </table>
+
+            </div>
+        </div>
+        <!-- Akhir Card Tabel -->
+
     </div>
-    <!-- Akhir Card Tabel -->
 
-</div>
-
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
-</html>                
+
+</html>
--- Dasawisma/Pelatihan.php
+++ Dasawisma/Pelatihan.php
@@ -1,79 +1,81 @@
 <?php
 
-    //Memanggil Koneksi Database
-    $server = "localhost";
-    $user = "root";
-    $password = "";
-    $database = "dasawisma";
+//Memanggil Koneksi Database
+$server = "localhost";
+$user = "root";
+$password = "";
+$database = "dasawisma";
 
-    $koneksi = mysqli_connect($server, $user, $password, $database)or die(mysqli_error($koneksi));
-    ?>
+$koneksi = mysqli_connect($server, $user, $password, $database) or die(mysqli_error($koneksi));
+?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Pelatihan</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
+
 <body>
-<div class="container">
+    <div class="container">
 
-    <!-- Awal Card Tabel -->
-    <div class="card mt-3" style="background-color: rgba(231, 116, 185, 0.6);">
-      <div class="card-header text-black text-center">
-        Pelatihan
-      </div>
-      <div class="card-body">
-        
-        <table class="table table-bordered table-striped">
-        <style type="text/css">
-        table td {
-            border-width: 1px;
-            padding: 8px;
-            border-style: solid;
-            border-color: black;
-            background-color: rgba(0,0,0,.03);
-        }
-        </style>
-            <tr>
-                <th>No.</th>
-                <th>Nama Pelatihan</th>
-                <th>NIK</th>
-                <th>Nama</th>
-                <th>Tahun</th>
-                <th>Keterangan</th>
-                <th>Dasawisma</th>
-                <th>Pilihan</th>
-            </tr>
-            <?php
-                $no = 1;
-                $tampil = mysqli_query($koneksi, "SELECT * from pelatihan order by idpelatihan desc");
-                while($data = mysqli_fetch_array($tampil)){
+        <!-- Awal Card Tabel -->
+        <div class="card mt-3" style="background-color: rgba(231, 116, 185, 0.6);">
+            <div class="card-header text-black text-center">
+                Pelatihan
+            </div>
+            <div class="card-body">
 
-            ?>
-            <tr>
-                <td><?=$no++;?></td>
-                <td><?=$data['NamaPelatihan']?></td>
-                <td><?=$data['NIK']?></td>
-                <td><?=$data['Nama']?></td>
-                <td><?=$data['Tahun']?></td>
-                <td><?=$data['Keterangan']?></td>
-                <td><?=$data['Dasawisma']?></td>
-                <td>
-                    <a href="editpelatihan.php?hal=edit&id=<?=$data['idpelatihan']?>" class="btn btn-warning"> Edit </a>
-                    <a href="index.php?hal=hapus&id=<?=$data['idpelatihan']?>" 
-                       onclick="return confirm('Apakah yakin ingin menghapus data ini?')" class="btn btn-danger"> Hapus </a>
-                </td>
-            </tr>
-        <?php } //penutup perulangan while ?>
-        </table>
+                <table class="table table-bordered table-striped">
+                    <style type="text/css">
+                        table td {
+                            border-width: 1px;
+                            padding: 8px;
+                            border-style: solid;
+                            border-color: black;
+                            background-color: rgba(0, 0, 0, .03);
+                        }
+                    </style>
+                    <tr>
+                        <th>No.</th>
+                        <th>Nama Pelatihan</th>
+                        <th>NIK</th>
+                        <th>Nama</th>
+                        <th>Tahun</th>
+                        <th>Keterangan</th>
+                        <th>Dasawisma</th>
+                        <th>Pilihan</th>
+                    </tr>
+                    <?php
+                    $no = 1;
+                    $tampil = mysqli_query($koneksi, "SELECT * from pelatihan order by idpelatihan desc");
+                    while ($data = mysqli_fetch_array($tampil)) {
 
-      </div>
+                    ?>
+                        <tr>
+                            <td><?= $no++; ?></td>
+                            <td><?= $data['NamaPelatihan'] ?></td>
+                            <td><?= $data['NIK'] ?></td>
+                            <td><?= $data['Nama'] ?></td>
+                            <td><?= $data['Tahun'] ?></td>
+                            <td><?= $data['Keterangan'] ?></td>
+                            <td><?= $data['Dasawisma'] ?></td>
+                            <td>
+                                <a href="editpelatihan.php?hal=edit&id=<?= $data['idpelatihan'] ?>" class="btn btn-warning"> Edit </a>
+                                <a href="hapuspelatihan.php?hal=hapus&id=<?= $data['idpelatihan'] ?>" onclick="return confirm('Apakah yakin ingin menghapus data ini?')" class="btn btn-danger"> Hapus </a>
+                            </td>
+                        </tr>
+                    <?php } //penutup perulangan while 
+                    ?>
+                </table>
+
+            </div>
+        </div>
+        <!-- Akhir Card Tabel -->
+
     </div>
-    <!-- Akhir Card Tabel -->
 
-</div>
-
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
-</html>
 
+</html>
