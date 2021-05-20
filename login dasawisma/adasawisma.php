11e766c8df7647a66968504938f35c099cda6c8f
Rahmatillah <rahma11@mhs.unsyiah.ac.id>
2021-05-20 09:31:44 +0700

    update halaman login

--- login dasawisma/adasawisma.php
+++ login dasawisma/adasawisma.php
@@ -7,28 +7,25 @@ $database = "dasawisma";
 
 $koneksi = mysqli_connect($server, $user, $password, $database) or die(mysqli_error($koneksi));
 
-if(isset($_POST['ilogin'])){
+if (isset($_POST['ilogin'])) {
     $NIK = $_POST['tnik'];
     $Password = $_POST['tpassword'];
-    
+
     $login = mysqli_query($koneksi, "select * from adasawisma where NIK='$NIK'");
     $cek = mysqli_num_rows($login);
 
-    if($cek === 1)
-    {
+    if ($cek === 1) {
         $fetch_pass = mysqli_fetch_assoc($login);
         $cek_password = $fetch_pass['Password'];
 
-        if($cek_password <> $Password){
-            echo"<script>alert('Password salah');</script>";
-        }
-        else{
+        if ($cek_password <> $Password) {
+            echo "<script>alert('Password salah');</script>";
+        } else {
             echo "<script>alert('Login Berhasil');
                 document.location='homeadasawisma.php'</script>";
         }
-    }else
-    {
-        echo"<script>alert('Username salah atau belum terdaftar');</script>";
+    } else {
+        echo "<script>alert('Username salah atau belum terdaftar');</script>";
     }
 }
 
@@ -36,43 +33,45 @@ if(isset($_POST['ilogin'])){
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anggota Dasawisma</title>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
-    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/Login.css">
 </head>
+
 <body>
-<br/><br/>
-    <img class="tengah" src="logo.jpg"/><br/>
-        <div class="data-form">
-                <form method="post" action="#">
-                    <h6 style="text-align:center"><b>DASAWISMA INDONESIA</b></h6>
-                    <h6 style="text-align:center"><b>Login sebagai Anggota Dasawisma :</b></h6>
-                    <div class="form-group">
-                        <label>NIK</label>
-                        <input type="number" name="tnik" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label>Password</label>
-                        <input type="password" name="tpassword" class="form-control" required>
-                    </div>
-                    <div class="btn_login">
-                    <button type="submit" name="ilogin" class="btn btn-block login">LOGIN</button>
-                    </div><br>
-                    <hr color="black">
-                    <center>
-                      <a class="link" href="">Forgot Password?</a><br>
-                      <a class="link" href="">Don't have an account? Register Now!</a>
-		                </center>
-                    
-                 </form>
+    <br /><br />
+    <img class="tengah" src="logo.jpg" /><br />
+    <div class="data-form">
+        <form method="post" action="#">
+            <h6 style="text-align:center"><b>DASAWISMA INDONESIA</b></h6>
+            <h6 style="text-align:center"><b>Login sebagai Anggota Dasawisma :</b></h6>
+            <div class="form-group">
+                <label>NIK</label>
+                <input type="number" name="tnik" class="form-control" required>
             </div>
-        </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="tpassword" class="form-control" required>
+            </div>
+            <div class="btn_login">
+                <button type="submit" name="ilogin" class="btn btn-block login">LOGIN</button>
+            </div><br>
+            <hr color="black">
+            <center>
+                <a class="link" href="#">Forgot Password?</a><br>
+                <a class="link" href="daftar.php">Don't have an account? Register Now!</a>
+            </center>
+
+        </form>
+    </div>
+    </div>
     </div>
 </body>
+
 </html>
